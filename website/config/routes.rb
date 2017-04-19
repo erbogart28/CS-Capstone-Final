92317574b817880c7bb42f05d2c3ef7221ec885f
Evan <gthrowaway28@gmail.com>
2017-04-19 20:21:34 +0000

    Basic admin dashboard landing page, controller, and routes added

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
     root 'home#land'  #root = default landing page for website
+
     get 'student/studentdashboard'
     get 'faculty/facultydashboard'
+    get 'admin/admindashboard'
+    
+
 end
