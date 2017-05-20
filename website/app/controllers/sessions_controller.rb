c3d4704b564f244e256df2020f3b45ff96b25450
Evan <gthrowaway28@gmail.com>
2017-05-20 05:46:32 +0000

    route user to specific dashboard depending on role

--- website/app/controllers/sessions_controller.rb
+++ website/app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ layout 'login'
   
   def create #CODE SHOULD BE ADDED TO DEFINE USER ROLES AND REDIRECT BASED ON ROLES | DO HERE OR IN HELPER
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) && user.admin?
       log_in user
-      redirect_back_or user
+      redirect_back_or admin_admindashboard_path
+    elsif user && user.authenticate(params[:session][:password]) && user.faculty?
+      log_in user
+      redirect_back_or faculty_facultydashboard_path
+    elsif user && user.authenticate(params[:session][:password]) && user.student?
+      log_in user
+      redirect_back_or student_studentdashboard_path
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
