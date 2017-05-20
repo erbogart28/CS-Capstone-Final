a5e61dc7470bfc049ac0b32bb322ecad0d196a95
Evan <gthrowaway28@gmail.com>
2017-05-20 05:29:08 +0000

    commented out older code for reference

--- website/app/controllers/when_ifs_controller.rb
+++ website/app/controllers/when_ifs_controller.rb
@@ -40,6 +40,8 @@ class WhenIfsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_when_if
+      # -      @when_if = WhenIf.find(params[:id])
+
       @when_if = WhenIf.find(params[:start_quarter, :degree_id, :course_load, :concentration])
     end
 
