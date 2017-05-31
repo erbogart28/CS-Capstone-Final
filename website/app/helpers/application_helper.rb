856a96da8e7085e9295a48cbe1f3077943415ca0
Josh Smith <theoperant@gmail.com>
2017-05-31 18:55:23 -0500

    Beginning cleanup of user profile page. Adds gravatar integration as well. Will most likely be refactored with student dashboard.

--- website/app/helpers/application_helper.rb
+++ website/app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
 	logger.debug("MSG TYPE:" + msg_type)
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
+      concat(content_tag(:div, message, class: "alert alert-dismissable fade in #{bootstrap_class_for(msg_type)}") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
               concat message 
             end)
