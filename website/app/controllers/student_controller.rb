1041690f05a12d1ce7fdd46c132eda6c5d30a885
Evan <gthrowaway28@gmail.com>
2017-06-01 00:14:22 +0000

    completed courses in student dash

--- website/app/controllers/student_controller.rb
+++ website/app/controllers/student_controller.rb
@@ -1,7 +1,5 @@
 class StudentController < ApplicationController
     def studentdashboard
-      @user = current_user
-      @courses_taken = Course.joins("INNER JOIN completed_courses ON courses.id = completed_courses.course_id").where("completed_courses.user_id = ?",params[:id])
-      @waive_course = CompletedCourse.new
+      @courses_taken = Course.joins("INNER JOIN completed_courses ON courses.id = completed_courses.course_id").where("completed_courses.user_id = ?",current_user)    
     end
 end
